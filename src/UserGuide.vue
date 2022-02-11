9f0d6a766d45f64153c69d17d45621aead9ebfaa
YaoLi3 <liyaoo6o@gmail.com>
2022-02-11 09:43:36 +0800

    add user guide page

---
+++ src/UserGuide.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <p> users guide powerpoints </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+        console.log("will replace it with url link");
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
+
