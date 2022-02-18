434812f880f1639c7cc23c198be2647813e4d466
YaoLi3 <liyaoo6o@gmail.com>
2022-02-18 08:49:56 +0800

    add color, roi, size

--- src/SingleCell.vue
+++ src/SingleCell.vue
@@ -21,27 +21,96 @@
         <el-menu-item index="17" @click.native="show_14dpa2"  >14dpa2 </el-menu-item>
       </el-menu>
     </div>
-    <el-container style="height: 750px; border: 1px solid #eee">
-      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-        <p> Color Configuration </p>
-        <el-table class="table" ref="multipleTable" style="width:160;" :data="tableData"
-          :show-header='false'
-          @selection-change="handleSelectionChange">
-            <el-table-column type="selection" width="25"> </el-table-column>
-            <el-table-column show-header='false' property="Celltype" label="Celltype" width="125">
-            </el-table-column>
-        </el-table>
-        <div style="margin-left:1%;width:25px">
-          <el-button @click="applyStatus">Highlight selected types</el-button>
+
+    <!-- Cell type color palette -->
+    <div v-draggable style='width:300px;height:400px;background-color:white;z-index:999;position:absolute;bottom:300;left:300;' v-if="isShowColorPalette">
+      <sketch-picker align='center' v-model="color" @input="colorValueChange"></sketch-picker>
+      <el-button align='right' style='width:50%;' @click='applyColor'>ChangeColor</el-button>
+      <el-button align='right' style='width:50%;' @click='closeColor'>Close</el-button>
+    </div>
+    <!-- end of color palette -->
+
+    <!-- Configuration Bar starts -->
+    <div style="margin-left:0%;background-color: rgb(238, 241, 246); border: 3px solid #eee;" >
+      
+      <!-- Cell type configuration menu start ... -->
+      <div class='inline_item'>
+        <el-button align='right' @click.native="openCTC" style='width:100%;z-index:9999;'>Cell Type Configuration</el-button>
+        <div class='parent' style='width:10px;'>
+          <div v-draggable class="child" style='width:500px;z-index:9999;background-color:white' v-if="!isHidden">
+            <el-table class="table" ref="multipleTable" style="width:160;" :data="tableData"
+              :show-header='false'
+              @row-click="getRowCelltype"
+              @selection-change="handleSelectionChange">
+              <el-table-column type="selection" width="25"> </el-table-column>
+              <el-table-column show-header='false' property="Celltype" label="Celltype" width="125"></el-table-column>
+              <el-table-column label="Display" width="160">
+                <el-button @click="showColorPalette">color palette</el-button>
+              </el-table-column>
+            </el-table>
+            <div style="margin-left:1%;width:25px">
+              <el-button @click="applyStatus">Highlight selected types</el-button>
+            </div>
+            <div style="margin-left:1%;width:25px">
+              <el-button @click="resetAll">Highlight all types</el-button>
+            </div>
+          </div> <!-- end of draggble div -->
+        </div> <!-- end of parent -->
+      </div>
+      <!-- Cell type configuration menu end ... -->
+      
+      <!-- ROI configuration menu start ... -->
+      <div class='inline_item' >
+        <el-button align='right' style='width:100%;' @click.native="openROI">ROI Configuration</el-button>
+        <div class='parent' style='width:10px; ' >
+          <div v-draggable class="child" style='width:500px;z-index:9999;background-color:white'  v-if="!isROIHidden">
+              <hr>
+              <p class='inline_item_tight'> Z gap scale : </p>
+              <el-input class='inline_item_tight' style='width:80px;height:50px;' type='number' v-model="tmp_z_scale" placeholder="1"></el-input>
+              <el-button class='inline_item' @click.native="changeZScale">Apply</el-button>
+              <hr>
+              <p class='inline_item_tight'> X min : </p>
+              <el-input class='inline_item_tight' style='width:80px;height:50px;' type='number' v-model="tmp_x_min" placeholder="0"></el-input>
+              <el-button class='inline_item_tight' @click.native="changeXMin">Apply</el-button>
+              <p class='inline_item'> X max : </p>
+              <el-input class='inline_item_tight' style='width:80px;height:50px;' type='number' v-model="tmp_x_max" placeholder="300"></el-input>
+              <el-button class='inline_item_tight' @click.native="changeXMax">Apply</el-button>
+              <hr>
+              <p class='inline_item_tight'> Y min : </p>
+              <el-input class='inline_item_tight' style='width:80px;height:50px;' type='number' v-model="tmp_y_min" placeholder="0"></el-input>
+              <el-button class='inline_item_tight' @click.native="changeYMin">Apply</el-button>
+              <p class='inline_item'> Y max : </p>
+              <el-input class='inline_item_tight' style='width:80px;height:50px;' type='number' v-model="tmp_y_max" placeholder="300"></el-input>
+              <el-button class='inline_item_tight' @click.native="changeYMax">Apply</el-button>
+              <hr>
+              <p class='inline_item_tight'> Z min : </p>
+              <el-input class='inline_item_tight' style='width:80px;height:50px;' type='number' v-model="tmp_z_min" placeholder="0"></el-input>
+              <el-button class='inline_item_tight' @click.native="changeZMin">Apply</el-button>
+              <p class='inline_item'> Z max : </p>
+              <el-input class='inline_item_tight' style='width:80px;height:50px;' type='number' v-model="tmp_z_max" placeholder="300"></el-input>
+              <el-button class='inline_item_tight' @click.native="changeZMax">Apply</el-button>
+              <hr>
+              <el-button class='inline_item' @click.native="resetROI">Reset ROI</el-button>
+              <el-button class='inline_item' @click.native="closeCTC">Close Configuration Panel</el-button>
+              <hr>
+          </div>
         </div>
-        <div style="margin-left:1%;width:25px">
-          <el-button @click="resetAll">Highlight all types</el-button>
-        </div>
-      </el-aside>
-      <el-main>
-        <v-chart ref="myecharts"  class="chart" :option="option" style="width:100%;height:700px" />
-      </el-main>
-    </el-container>
+      </div>
+      <!-- ROI configuration menu end ... -->
+      <!-- switch symbol size start -->
+      <div class="inline_item">
+        <span class="inline_item" style="z-index:1;">Symbol size :</span>
+        <el-slider class="inline_item" style="width:200px;z-index:1;" v-model="symbolSize"
+           :step="1" :min="2" :max="10" @change="refresh" show-stops>
+         </el-slider>
+      </div>
+      <!-- switch symbol size end -->
+    </div>
+    <!-- Configuration Bar ends -->
+
+    <div>
+      <v-chart ref="myecharts"  class="chart" :option="option" style="width:100%;height:700px" />
+    </div>
   </div>
 </template>
 
@@ -51,6 +120,9 @@
   import * as echarts from 'echarts';
   import 'echarts-gl';
   import VChart from "vue-echarts";
+  import { Draggable } from 'draggable-vue-directive';
+  import { Sketch } from 'vue-color'
+
   // confs
   var SC_URL="http://49.235.68.146/single_cell/"
   var COLOR_ALL = require('../confs/discret_color.js');
@@ -60,10 +132,38 @@
   export default {
     name : "singlecell",
     components: {
-        VChart
+      VChart,
+      'sketch-picker': Sketch
+    },
+    directives: {
+      Draggable,
     },
     data(){
       return {
+        symbolSize:2,
+        // configure panels
+        isHidden: true,
+        isROIHidden:true,
+        isShowColorPalette: false,
+        color: '',
+        currentCellID: '',
+        current_color_all: COLOR_ALL,
+        //------------roi confs------
+        z_scale:1,
+        tmp_z_scale:1,
+        // roi
+        tmp_x_min:0,
+        tmp_y_min:0,
+        tmp_z_min:0,
+        tmp_x_max:300,
+        tmp_y_max:300,
+        tmp_z_max:300,
+        x_min:0,
+        y_min:0,
+        z_min:0,
+        x_max:300,
+        y_max:300,
+        z_max:300,
         // empty charts
         option: {
            backgroundColor:'#000000',
@@ -84,9 +184,164 @@
         // drawing cache
         curr_name : null,
         curr_data : null,
+        rawdata: null,
       }; // end of data return
     },
     methods: {
+      //-------------refresh  -------------------//
+     refresh(){
+       this.option=this.getOption();
+     },
+     //----------- color palette start -------------//
+     showColorPalette(){
+       if (this.isShowColorPalette){
+         this.isShowColorPalette = false;
+       }else{
+         this.isShowColorPalette = true;
+       }
+     },
+     getRowCelltype(row){
+       // 1. get row id when click on row (except for selection box)
+       this.currentCellID = row.ID;
+       console.log('getrow '+this.currentCellID);
+       return row.ID;
+     },
+     colorValueChange (val) {
+      this.color = val.hex;
+      },
+     applyColor(){
+       // change color when click row
+       // only apply changes when click button
+       this.current_color_all[this.currentCellID] = this.color;
+       this.option=this.getOption();
+     },
+     closeColor(){
+       this.isShowColorPalette = false;
+     },
+      //-------------color palette ends------------------//
+      //-------------conf panels-------------------------//
+      openCTC(){
+        this.isROIHidden = true;
+        this.isUMAPHidden = true;
+        this.isHidden = ! this.isHidden;
+        if (this.isShowColorPalette){
+          this.isShowColorPalette = false;
+        }
+      },
+      openROI(){
+        this.isHidden = true;
+        this.isUMAPHidden = true;
+        this.isROIHidden = ! this.isROIHidden;
+      },
+      closeCTC(){
+        this.isHidden = true;
+        this.isROIHidden = true;
+        this.isUMAPHidden = true;
+        this.isShowColorPalette = false;
+      },
+      //-------------conf panel ends------------------------//
+      //-------------configure ROI start -------------------//
+      resetROIdata(){
+        // reset-roi
+        this.z_scale = 1;this.tmp_z_scale=1;
+        this.x_min = 0;this.y_min = 0;this.z_min = 0;this.tmp_x_min=0;this.tmp_y_min=0;this.tmp_z_min=0;
+        this.x_max = this.getWidth();this.y_max = this.getHeight(); this.z_max = this.getDepth();
+        this.tmp_x_max = this.x_max; this.tmp_y_max = this.y_max; this.tmp_z_max = this.z_max;
+      },
+      resetROI(){
+        this.resetROIdata();
+        this.curr_data = this.rawdata;
+        this.option=this.getOption();
+      },
+      updateJsonData(){
+        console.log("update json data");
+        var curr_draw_datas = [];
+        for(var i =0;i<this.tableData.length;i++)
+            curr_draw_datas.push([['x','y','z']]);
+        for(var i =0;i<this.tableData.length;i++){
+          for(var j =1;j<this.rawdata[i].length; j++){
+             var info = this.rawdata[i][j];
+             if( info[0]<this.x_min) continue;
+             if( info[1]<this.y_min) continue;
+             if( info[2]<this.z_min) continue;
+             if( info[0]>this.x_max) continue;
+             if( info[1]>this.y_max) continue;
+             if( info[2]>this.z_max) continue;
+             curr_draw_datas[i].push(info)
+          }
+        }
+        this.curr_data = curr_draw_datas;
+      },
+      changeXMin(){
+        console.log("change x min");
+        if(this.x_min != this.tmp_x_min){
+          if(this.tmp_x_min<0)                          this.x_min = 0;
+          else if (this.tmp_x_min>this.x_max-1)         this.x_min = this.x_max-1;
+          else if (this.tmp_x_min>this.getWidth()-1)    this.x_min = this.getWidth()-1;
+          else                                          this.x_min = this.tmp_x_min;
+          this.updateJsonData();
+          this.option=this.getOption();
+        }
+      },
+      changeXMax(){
+        if(this.x_max != this.tmp_x_max){
+          if(this.tmp_x_max<1)                          this.x_max = 1;
+          else if (this.tmp_x_max<Number(this.x_min)+1) this.x_max = Number(this.x_min) +1;
+          else if (this.tmp_x_max>this.getWidth())      this.x_max = this.getWidth();
+          else                                          this.x_max = this.tmp_x_max;
+          console.log(this.x_max);
+          this.updateJsonData();
+          this.option=this.getOption();
+        }
+      },
+      changeYMin(){
+        if(this.y_min != this.tmp_y_min){
+          if(this.tmp_y_min<0)                          this.y_min = 0;
+          else if (this.tmp_y_min>this.y_max-1)         this.y_min = this.y_max-1;
+          else if (this.tmp_y_min>this.getHeight()-1)   this.y_min = this.getHeight()-1;
+          else                                          this.y_min = this.tmp_y_min;
+          console.log(this.y_min);
+          this.updateJsonData();
+          this.option=this.getOption();
+        }
+      },
+      changeYMax(){
+        if(this.y_max != this.tmp_y_max){
+          if(this.tmp_y_max<1)                          this.y_max = 1;
+          else if (this.tmp_y_max<Number(this.y_min)+1) this.y_max = Number(this.y_min) +1;
+          else if (this.tmp_y_max>this.getHeight())     this.y_max = this.getHeight();
+          else                                          this.y_max = this.tmp_y_max;
+          console.log(this.y_max);
+          this.updateJsonData();
+          this.option=this.getOption();
+        }
+      },
+      changeZMin(){
+        if(this.z_min != this.tmp_z_min){
+          if(this.tmp_z_min<0)                          this.z_min = 0;
+          else if (this.tmp_z_min>this.z_max-1)         this.z_min = this.z_max-1;
+          else if (this.tmp_z_min>this.getDepth()-1)    this.z_min = this.getDepth()-1;
+          else                                          this.z_min = this.tmp_z_min;
+          this.updateJsonData();
+          this.option=this.getOption();
+        }
+      },
+      changeZMax(){
+        if(this.z_max != this.tmp_z_max){
+          if(this.tmp_z_max<1)                          this.z_max = 1;
+          else if (this.tmp_z_max<Number(this.z_min)+1) this.z_max = Number(this.z_min) +1;
+          else if (this.tmp_z_max>this.getDepth())      this.z_max = this.getDepth();
+          else                                          this.z_max = this.tmp_z_max;
+          this.updateJsonData();
+          this.option=this.getOption();
+        }
+      },
+      changeZScale(){
+        this.z_scale = this.tmp_z_scale;
+        this.option = this.getOption();
+      },
+      //-------------configure ROI end -------------------//
+
       //-------------3d box conf start-------------------//
       getWidth(){
         return idvd_conf['label_'+this.curr_name].x ;
@@ -176,6 +431,7 @@
           var type_id = this.name_id_map[type_name];
           curr_draw_datas[type_id].push([curr_item[0],curr_item[1],curr_item[2]]);
         } // end of for _data
+        this.rawdata = curr_draw_datas;
         this.curr_data = curr_draw_datas;
       },
       getOption() {
@@ -213,13 +469,14 @@
              used_color_masks = this.colored_cache;
           }
           console.log('start series');
-          console.log(used_color_masks);
+          //console.log(used_color_masks);
           for( var i = 0 ; i<used_color_masks.length; i++ )
           {
             var curr_legend_name = this.tableData[i].Celltype;
-            console.log(curr_legend_name);
+            //console.log(curr_legend_name);
             legend_list.push(curr_legend_name);
-            curr_color = COLOR_ALL[i];
+            //curr_color = COLOR_ALL[i];
+            curr_color = this.current_color_all[i];
             curr_alpha = 0.75;
             // console.log('2');
             if(used_color_masks[i] == 0){
@@ -233,7 +490,7 @@
                 type : 'scatter3D',
                 dimensions: [ 'x','y','z' ],
                 data: the_data,
-                symbolSize: 2,
+                symbolSize: this.symbolSize,
                 itemStyle: {
                   borderWidth: 0,
                   color: curr_color,
@@ -243,10 +500,19 @@
             series_list.push(one_series);
           } // end of for showd_clusters.length
           console.log('end series');
-          console.log(legend_show);
-          var xmax = this.getWidth();
-          var ymax = this.getHeight();
-          var zmax = this.getDepth();
+          //console.log(legend_show);
+          //var xmax = this.getWidth();
+          //var ymax = this.getHeight();
+          //var zmax = this.getDepth();
+          var used_xmin = 0;
+          var used_xmax = this.getWidth();
+          var used_ymin = 0;
+          var used_ymax = this.getHeight();
+          var used_zmin = 0;
+          var used_zmax = this.getDepth();
+          var boxWidth  = used_xmax;
+          var boxHeight = used_zmax*this.z_scale;
+          var boxDepth  = used_ymax;
           var opt={
             backgroundColor:'#000000',
             title : {
@@ -259,22 +525,22 @@
               name: 'x',
               scale:1,
               type: 'value',
-              min:0,
-              max:xmax,
+              min: used_xmin,
+              max: used_xmax,
             },
             yAxis3D: {
               name: 'y',
               scale:1,
               type: 'value',
-              min:0,
-              max:ymax,
+              min: used_ymin,
+              max: used_ymax,
             },
             zAxis3D: {
               name: 'z',
               scale:1,
               type: 'value',
-              min:0,
-              max:zmax,
+              min: used_zmin,
+              max: used_zmax,
             },
             legend :{
               data: legend_list,
@@ -284,9 +550,9 @@
               },
             },
             grid3D: {
-              boxWidth: xmax,
-              boxHeight: zmax,
-              boxDepth: ymax,
+              boxWidth: boxWidth,
+              boxHeight: boxHeight,
+              boxDepth: boxDepth,
               axisLine: {
                 lineStyle: {
                   color: '#fff'
@@ -320,5 +586,23 @@
 .chart {
   width: 100%;
   height: 800px;
+}
+.parent {
+  position: relative;
+}
+.child {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.inline_item_tight {
+  display: inline-block;
+  margin-left: 3px;
+  vertical-align: middle;
+}
+.inline_item {
+  display: inline-block;
+  margin-left: 20px;
+  vertical-align: middle;
 }
 </style>
