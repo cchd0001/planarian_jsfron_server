02303db66cd91565d6eded5becfea570aa71b0a8
YaoLi3 <liyaoo6o@gmail.com>
2022-10-10 10:45:30 +0800

    resolve conflict

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,9 @@
   <div id="app">
     <header>
       <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" active-text-color="#409eff" style="margin: 0px;">
-<<<<<<< HEAD
-        <el-menu-item index="1">Home</el-menu-item>
-        <el-submenu index="2" active-text-color="#409eff">
-          <template slot="title">3D Atlas Visuliazationabc</template>
-            <el-menu-item index="2-1">Cell Type Atlas</el-menu-item>
-            <el-menu-item index="2-2">Gene Expression Atlas</el-menu-item>
-            <el-menu-item index="2-3">Single-cell-label-transfered Cell Atlas</el-menu-item>
-        </el-submenu>
-        <el-menu-item index="3" disabled></el-menu-item>
-        <el-submenu index="3" active-text-color="#409eff">
-          <template slot="title">Data Analysis</template>
-            <el-menu-item index="3-1">Multi-gene-pattern</el-menu-item>
-        </el-submenu>
-        <el-menu-item index="4">User Guide</a></el-menu-item>
-        <el-menu-item index="5" disabled>About Us</a></el-menu-item>
-=======
         <el-menu-item index="1">Planarian atlas 3D viewer</el-menu-item>
         <el-menu-item index="2">Planarian atlas home</el-menu-item>
         <el-menu-item index="3" disabled >About us</el-menu-item>
->>>>>>> 549d3c3dc3ae5d97283e3b076ed0c1faf6544ce8
       </el-menu>
     </header>
     <article>
@@ -81,8 +64,6 @@ export default {
   font-family: Arial;
   text-align: center;
   margin-top: -8px;
-<<<<<<< HEAD
-=======
 }
 hr{
     border:2px solid #ccc;
@@ -92,12 +73,5 @@ header{
     z-index: 999;
     top: 0;
     margin-bottom: 8px;
->>>>>>> 549d3c3dc3ae5d97283e3b076ed0c1faf6544ce8
 }
-header{
-    position: sticky;
-    z-index: 999;
-    top: 0;
-    margin-bottom: 8px;
-    }
 </style>
